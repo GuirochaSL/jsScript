26a7b41fab708dbd87ca5dbf77c2b650cf063f99
Guilherme Rocha <guilherme.rocha@sawluz.com.br>
2023-03-13 09:57:53 -0300

    aula variaveis e conversoes

---
+++ bool.js
@@ -0,0 +1,11 @@
+const contpaga = false;
+const cont = true;
+
+console.log (0 == false)
+console.log ("" == false)
+console.log (1 == true)
+
+const numero = 10;
+const numeroString = "10"
+
+console.log(numero+Number(numeroString))
